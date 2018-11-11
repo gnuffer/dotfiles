4a58b658ed3a42b75e1a886579ba50797cacd726
gnuffer <gfnuffer@gmail.com>
2018-11-11 16:21:27 +0100

    Edit installation shell script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,3 +1,33 @@
 #! /bin/bash
 
+################
+# This script creates symlinks from ~ to the dotfiles in ~/dotfiles.
+################
+
+#### Variables
+
+dir=~/dotfiles   # dotfiles directory
+olddir=~/dotfiles_old    # backup directory for old dotfiles
+files="bash_profile emacs tmux.conf vimrc"    # list of dotfiles
+
+####
+
+# create olddir in ~
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo " ... done"
+
+# change to the dotfiles directory
+echo "changing to $dir"
+cd $dir
+echo " ... done"
+
+# move any existing dotfiles in ~ to olddir, then create symlinks from ~ to the files in dir
+for file in $files; do
+	echo "Moving $file from ~ to $olddir"
+	mv ~/.$file $olddir
+	echo "Creating symlink to $file in home directory"
+	ln -s $dir/$file ~/.$file
+done
+echo " ... done"
 
